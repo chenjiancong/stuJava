0cc40fe77fbe7d43c39b026a6282407cc18bcd24
Work_PC <chenjiancong@gmail.com>
2019-11-28 15:16:42 +0800

    DBTest

---
+++ src/com/stuJava/DBTest.java
@@ -0,0 +1,26 @@
+package com.stuJava;
+import java.sql.*;
+
+public class DBTest {
+    public static void main(String[] args) {
+        try {
+            String driver="org.mariadb.jdbc.Driver";
+            String url ="jdbc:mariadb://localhost:3306/test";
+            String user="root";
+            String pass="jack";
+
+            Class.forName(driver);
+            Connection conn=DriverManager.getConnection(url,user,pass);
+            Statement stmt=conn.createStatement();
+
+            ResultSet rs=stmt.executeQuery("select * from school.stu");
+            while (rs.next()){
+                System.out.println(rs.getInt(1)+"\t\t"+rs.getString(2)
+                +"\t\t"+rs.getString(3));
+            }
+//            System.out.println("Success");
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+}
